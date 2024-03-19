bf2f99403793b955df44b41ef36f5e6415cd7ee2
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-03-19 09:07:42 -0500

    Configure and set up database framework

---
+++ src/schemas/schemas-index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// 3rd party resources
+const { Sequelize } = require('sequelize');
+
+// Esoteric resources
+
+
+// Connect to database for testing purposes, OR connect to database from env
+const DATABASE_URL = process.env.NODE_ENV === 'test'
+  ? 'sqlite:memory'
+  : process.env.DATABASE_URL;
+
+// Configure database with dialect options
+const DATABASE_CONFIGURATION = process.env.NODE_ENV === 'production' ? {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+} : {};
+
+// Initialize single instance of Sequelize with databae configuration
+const sequelize = new Sequelize(DATABASE_URL, DATABASE_CONFIGURATION);
+
+module.exports = {
+  database: sequelize,
+};
