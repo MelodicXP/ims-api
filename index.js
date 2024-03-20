2b936c8560073b9b881a3c4af24b3373141805e2
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-03-20 07:56:37 -0500

    Fix file path

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const { startServer } = require('./src/server');
 
-const { database } = require('./src/Database/database-config');
+const { database } = require('./src/database/database-config');
 
 async function initializeApp() {
   try {
