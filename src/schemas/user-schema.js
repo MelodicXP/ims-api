2391a9dffbfda83af164162b262ffa1d11d644ba
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-03-20 07:59:03 -0500

    Add user schema framework

---
+++ src/schemas/user-schema.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const properties = require('./user-properties');//import user properties here
+const { hashedPassword } = require('../utilities/hash-password');
+
+const userSchema = (database, DataTypes) => {
+  // Prepare the properties for sequelize database by passing DataTypes
+  const userProperties = properties(DataTypes);
+
+  const schema = database.define('Users', userProperties);
+
+  schema.beforeCreate(async (user) => {
+    user.password = await hashedPassword(user.password);
+  });
+};
+
+module.exports = userSchema;
