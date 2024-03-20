d0bc40045c1478826c77a6676446f692850e4ef8
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-03-20 09:20:31 -0500

    Add basic authorization capability

---
+++ src/utilities/basic-auth.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const bcrypt = require('bcrypt');
+const CRUD = require('./crud-interface');
+
+async function authenticateBasic (userModel, username, password) {
+
+  // Insantiate CRUD with userModel
+  const CRUD_Interface = new CRUD(userModel);
+
+  try {
+
+    const user = await CRUD_Interface.get({ username });
+
+    if(!user) {
+      throw new Error('User not found');
+    }
+
+    // Compare password provided to password from database
+    const validPassword = await bcrypt.compare(password, user.password);
+
+    if(validPassword) {
+      return user;
+    } else {
+      throw new Error('Invalid User');
+    }
+
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+module.exports = authenticateBasic;
