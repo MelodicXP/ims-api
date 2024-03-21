0d9062cc579b70af2aab10a2dfae65a7b1779a99
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-03-21 12:32:42 -0500

    Fix error user not being created

--- src/schemas/user/user-schema.js
+++ src/schemas/user/user-schema.js
@@ -1,15 +1,15 @@
 'use strict';
 
-const properties = require('./user-properties');
+const userProperties = require('./user-properties');
 const { hashedPassword } = require('../../utilities/hash-password');
 const authenticateBasic = require('../../authentication/basic-auth');
 const authenticateToken = require('../../authentication/bearer-token-auth');
 
 const userSchema = (database, DataTypes) => {
   // Prepare the properties for sequelize database by passing DataTypes
-  const userProperties = properties(DataTypes);
+  const properties = userProperties(DataTypes);
 
-  const schema = database.define('Users', userProperties);
+  const schema = database.define('Users', properties);
 
   schema.beforeCreate(async (user) => {
     user.password = await hashedPassword(user.password);
@@ -18,6 +18,8 @@ const userSchema = (database, DataTypes) => {
   schema.authenticateBasic = (username, password) => authenticateBasic(schema, username, password);
 
   schema.authenticateToken = (token) => authenticateToken(schema, token);
+
+  return schema;
 };
 
 module.exports = userSchema;
