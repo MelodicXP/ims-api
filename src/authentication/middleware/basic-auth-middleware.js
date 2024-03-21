85eeb89b5d387a51252d1cff4d2696ca14cec783
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-03-21 09:44:12 -0500

    Add basic authentication middleware

---
+++ src/authentication/middleware/basic-auth-middleware.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const base64 = require('base-64');
+const { users } = require('../../schemas/models-and-collections');
+
+function decodeBasicAuthHeader (authHeader) {
+  const encodedString = authHeader.split(' ').pop;
+  const decodedString = base64.decode(encodedString);
+  const [username, password] = decodedString.split(':');
+  return { username, password };
+}
+
+function authenticationErrorResponse(res) {
+  res.status(403).json({ message: 'Invalid Login'});
+}
+
+module.exports = async (req, res, next) => {
+  const header = req.headers.authorization;
+
+  if (!header || !header.startsWith('Basic')) {
+    return authenticationErrorResponse(res);
+  }
+  
+  try {
+    const { username, password } = decodeBasicAuthHeader(header);
+    req.user = await users.authenticateBasic(username, password);
+    next();
+  } catch (error) {
+    console.error(error);
+    authenticationErrorResponse(res);
+  }
+};
