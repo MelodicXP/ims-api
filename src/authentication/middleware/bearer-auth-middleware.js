7cfada2b7645b9770cc6b975334b13660a94190f
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-03-22 11:29:20 -0500

    Add bearer auth middleware

---
+++ src/authentication/middleware/bearer-auth-middleware.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const models = require('../../models/database-models');
+const { users } = models;
+
+function extractTokenFromHeader(authHeader) {
+  const token = authHeader.split(' ').pop();
+  return token;
+}
+
+function sendAuthenticationErrorResponse(res) {
+  res.status(403).json({ message: 'Invalid Login'});
+}
+
+async function validateUserToken (token) {
+  try {
+    const user = await users.authenticateUserToken(token);
+    if (!user) {
+      throw new Error('Invalid Token');
+    }
+    return user;
+  } catch (error) {
+    throw new Error('Authentication failed');
+  }
+}
+
+module.exports = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return sendAuthenticationErrorResponse(res);
+  }
+
+  try {
+    const token = extractTokenFromHeader(authHeader);
+    const user = await validateUserToken(token);
+    req.user = user; // User token validated
+    req.token = token; // Attach token to request
+    next();
+
+  } catch (error) {
+    console.error(error);
+    sendAuthenticationErrorResponse(res);
+  }
+};
