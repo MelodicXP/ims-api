c4a7041c2bf8f3ac71e6635c08051ea7bd0292c2
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-03-23 15:26:09 -0500

    Refactor code

--- src/routes/v1-routes.js
+++ src/routes/v1-routes.js
@@ -3,31 +3,56 @@
 const express = require('express');
 
 const crudInterfaces = require('../database/database-crud-interfaces');
+const verifyBearerToken = require('../authentication/middleware/bearer-auth-middleware');
+const requirePermission = require('../authentication/middleware/access-control-list-middleware');
+
 
 const router = express.Router();
 
-async function handleGetAll(req, res) {
-  let allRecords = await req.model.get();
-  res.status(200).json(allRecords);
+async function handleGetAll(req, res, next) {
+  try {
+    let allRecords = await req.model.get();
+    res.status(200).json(allRecords);
+  } catch (error) {
+    next(error);
+  }
 }
 
-async function handleGetOne(req, res) {
-  const id = req.params.id;
-  let theRecord = await req.model.get(id);
-  res.status(200).json(theRecord);
+async function handleGetOne(req, res, next) {
+  try {
+    const id = req.params.id;
+    let theRecord = await req.model.get({ id: id });
+    if(!theRecord) {
+      return res.status(404).json({ message: 'Record not found'});
+    }
+    res.status(200).json(theRecord);
+  } catch (error) {
+    next(error);
+  }
 }
 
-async function handleCreate(req, res) {
-  let obj = req.body;
-  let newRecord = await req.model.create(obj);
-  res.status(201).json(newRecord);
+async function handleCreate(req, res, next) {
+  try {
+    let obj = req.body;
+    let newRecord = await req.model.create(obj);
+    res.status(201).json(newRecord);
+  } catch (error) {
+    next(error);
+  }
 }
 
-async function handleUpdate(req, res) {
-  const id = req.params.id;
-  const obj = req.body;
-  let updatedRecord = await req.model.update(id, obj);
-  res.status(200).json(updatedRecord);
+async function handleUpdate(req, res, next) {
+  try {
+    const id = req.params.id;
+    const obj = req.body;
+    let updatedRecord = await req.model.update(id, obj);
+    if(!updatedRecord) {
+      return res.status(404).json({ message: 'Record not found'});
+    }
+    res.status(200).json(updatedRecord);
+  } catch (error) {
+    next(error);
+  }
 }
 
 async function handleDelete(req, res, next) {
@@ -51,12 +76,10 @@ router.param('model', (req, res, next) => {
   }
 });
 
-router.get('/:model', handleGetAll);
-router.get('/:model/:id', handleGetOne);
-router.post('/:model', handleCreate);
-router.put('/:model/:id', handleUpdate);
-router.delete('/:model/:id', handleDelete);
-
-
+router.get('/:model', verifyBearerToken, handleGetAll);
+router.get('/:model/:id', verifyBearerToken, handleGetOne);
+router.post('/:model', verifyBearerToken, requirePermission('create'), handleCreate);
+router.put('/:model/:id', verifyBearerToken, requirePermission('update'), handleUpdate);
+router.delete('/:model/:id', verifyBearerToken, requirePermission('delete'), handleDelete);
 
 module.exports = router;
