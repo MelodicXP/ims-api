6420f9a495872d9145019b9868888eec21a197d4
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2024-03-26 09:56:38 -0500

    CDefine proper category model

--- src/models/categories/category-model.js
+++ src/models/categories/category-model.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const getCategoryProperties = require('../items/item-properties');
+const getCategoryProperties = require('../categories/category-properties');
 
 const defineItemModel = (sequelize, DataTypes) => {
   const categoryModelProperties = getCategoryProperties(DataTypes);
-  const category = sequelize.define('Items', categoryModelProperties);
+  const category = sequelize.define('Category', categoryModelProperties);
   return category;
 };
 
